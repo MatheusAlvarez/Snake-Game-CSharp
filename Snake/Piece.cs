e2822b0e5e489c08d8a88998370b6789405f15b2
Matheus Maia Alvarez <88752578+MatheusAlvarez@users.noreply.github.com>
2021-11-25 09:22:21 -0300

    Add files via upload

---
+++ Snake/Piece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    class Piece : Label
+    {
+        public Piece(int x, int y)
+        {
+            Location = new System.Drawing.Point(x, y);
+            Size = new System.Drawing.Size(20, 20);
+            BackColor = Color.Green;
+            Enabled = false;
+        }
+    }
+}
